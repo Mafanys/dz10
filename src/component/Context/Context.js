d1b5fd4bebc078c2e9b2b09d196b2df73acc31c7
Sasha <sashadrifting48@gmail.com>
2022-05-02 13:12:38 +0300

    first commit

---
+++ src/component/Context/Context.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { MyContext } from "../../context-api/MyContext";
+
+class Context extends React.Component{
+  constructor(){
+    super();
+    this.changeColor = this.changeColor.bind(this);
+  }
+  state = {
+    isChange: false
+  }
+
+  changeColor(){
+    this.setState({isChange: !this.state.isChange})
+    console.log(this.state.isChange);
+  }
+    
+  
+
+  render(){
+    
+    return(
+      <div>
+        <MyContext.Consumer>
+          {style =>{
+            const {backgroundBlack,colorWhite,colorBlack,backgroundBlue, text} = style;                      
+            return (
+              <div>
+                <h2 style={this.state.isChange?
+                {background: backgroundBlack, color: colorWhite}
+                : {background: backgroundBlue, color: colorBlack}
+              }>{text}</h2>
+                <button onClick={this.changeColor}>Change</button>
+              </div>
+            )
+          }}
+        </MyContext.Consumer>
+      </div>
+    )
+  }
+}
+
+export default Context;
---
+++ src/context-api/MyContext.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const style ={
+  backgroundBlack: "black",
+  backgroundBlue: "Blue",
+  colorBlack: "black",
+  colorWhite: "white",
+  text: "Hello world"
+}
+
+
+export  const MyContext = React.createContext(style);
